fbbba44b545fd94f7aba2109d9d62d7eba0764f5
Felipe C. dos Santos <felipe.santos@hbsis.com.br>
2019-03-02 14:14:57 -0300

    Adicionado botao menu para Sair e retorna resumo entre despesa e receita

--- OrganizzeClone/app/src/main/java/com/example/felipesantos/organizzeclone/activity/PrincipalActivity.java
+++ OrganizzeClone/app/src/main/java/com/example/felipesantos/organizzeclone/activity/PrincipalActivity.java
@@ -2,27 +2,41 @@ package com.example.felipesantos.organizzeclone.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.felipesantos.organizzeclone.R;
 import com.example.felipesantos.organizzeclone.config.ConfiguracaoFirebase;
+import com.example.felipesantos.organizzeclone.helper.Base64Custom;
+import com.example.felipesantos.organizzeclone.model.Usuario;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
+import java.text.DecimalFormat;
+
 public class PrincipalActivity extends AppCompatActivity {
 
-    private Button btSair;
-    private FirebaseAuth autenticacao;
     private MaterialCalendarView calendarView;
     private TextView textSaudacao, textSaldo;
+    private double despesaTotal =0.0;
+    private double receitaTotal =0.0;
+    private double resumoUsuario =0.0;
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private DatabaseReference databaseReference = ConfiguracaoFirebase.getFirebaseDatabase();
 
 
     @Override
@@ -53,32 +67,59 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });
 
-       /*
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
+//        Recupera resumo de valores do usuario
+        reuperarResumo();
 
-/*        btSair = findViewById(R.id.btSair);
-
-        btSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deslogarUsuario();
-            }
-        });*/
     }
 
-/*    public void deslogarUsuario(){
-        autenticacao = ConfiguracaoFirebase.GET_FIREBASE_AUTENTICACAO();
-        autenticacao.signOut();
-        finish();
-    }*/
+    public void reuperarResumo() {
+        String emailUssuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUssuario);
+        DatabaseReference usuarioRef = databaseReference.child("usuarios")
+                .child(idUsuario);
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+
+                despesaTotal = usuario.getDespesaTotal();
+                receitaTotal = usuario.getReceitaTotal();
+                resumoUsuario = receitaTotal - despesaTotal;
+
+                DecimalFormat decimalFormat = new DecimalFormat("0.##");
+                String resultadoFormatado = decimalFormat.format(resumoUsuario);
+
+                textSaudacao.setText("Olá, " + usuario.getNome());
+                textSaldo.setText("R$ " + resultadoFormatado);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                autenticacao.signOut();
+                finish();
+                //startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     public void  adicionarDespesa(View v){
 
